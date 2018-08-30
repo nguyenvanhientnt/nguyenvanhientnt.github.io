7f06a8c6ee9a46b89c64ebb8b52f821cb5709a63
nguyenvanhientnt <hanoimatbao@gmail.com>
2018-08-31 00:55:34 +0700

    creat memory card

--- memory card/js/script.js
+++ memory card/js/script.js
@@ -1,11 +1,55 @@
 let img = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16'];
-img = img.concat(img); //nhân đôi mảng.
-shuffle(img); //tạo hàm random current đổi vị trước khi load css
-let html = '';
-for(let i = 0; i<=img.length-1; i++){
-    html += '<div class="card">' + '<div class="before"><img src="../image/'+img[i]+'.jpg"></div>' + '<div class="after"><img src="../image/before2.jpg"></div>' + '</div>';
+let current = null;
+
+function flip(card){
+    $(card).toggleClass('flipped');
+    $(card).css('pointer-events', 'none');
+
+    if(!current){
+        current = $(card);
+
+    } else{
+        if(current.attr('data-name') != $(card).attr('data-name')){
+            //khác nhau
+            $('.card').css('pointer-events', 'none');
+
+            setTimeout(function(){
+                $(card).toggleClass('flipped');
+                current.toggleClass('flipped');
+                document.getElementById('incorrect-sound').play();
+                current = null;
+            }, 500);
+            
+            console.log('khac nhau');
+            setTimeout(function(){
+                $('.card').css('pointer-events', 'auto');
+                
+            console.log('khac nhau2');
+
+            }, 800);
+        }else{
+            // giống nhau
+            setTimeout(function(){
+                $(card).css('visibility', 'hidden');
+                current.css('visibility', 'hidden');
+                $(card).css('opacity', '0');
+                current.css('opacity', '0');
+                current = null;
+            }, 200);
+            // document.getElementById('').play();
+        }
+    }
 }
-$('.can').html(html);
+
+$(function(){
+    img = img.concat(img); //nhân đôi mảng tạo ra các lá bài
+    shuffle(img); //tạo hàm random current đổi vị trí
+    let html = '';
+    for(let i = 0; i<=img.length-1; i++){
+        html += '<div class="card" data-name="'+img[i]+'" onclick="flip(this)">' + '<div class="before"><img src="../image/'+img[i]+'.jpg"></div>' + '<div class="after"><img src="../image/before2.jpg"></div>' + '</div>';
+    }
+    $('.can').html(html);
+});
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
