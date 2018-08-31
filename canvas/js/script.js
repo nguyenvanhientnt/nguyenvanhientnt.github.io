589d869a23cc9f10d09dfa59d8bdccb1a540b7d1
nguyenvanhientnt <hanoimatbao@gmail.com>
2018-09-01 00:52:04 +0700

    creat canvas

---
+++ canvas/js/script.js
@@ -0,0 +1,45 @@
+
+
+
+// window.onload = funtion(){
+// 	draw();
+// }
+
+
+// let canvas = document.getElementById('game');
+// let ctx = canvas.getContext('2d');
+
+// let bird = new Image();
+// let bg = new Image();
+// let fg = new Image();
+// let pipeNorth = new Image();
+// let pipeSouth = new Image();
+
+// bird.src = "../image/bird.png";
+// bg.src = "../image/bg.jpg";
+// fg.src = "../image/fg.jpg";
+// pipeSouth.src = "../image/pipesouth.png";
+// pipeNorth.src = "../image/pipenorth.png";
+
+// let gap = 75;
+// let contant = pipeNorth.height+gap;
+
+// let bX = 50;
+// let bY = 150;
+// let gravity = 5;
+
+// document.addEventListener("keydown",moveUp);
+
+// function moveUp(){
+//     bY-=20;
+// }
+
+
+// function draw(){
+//   ctx.drawImage(bg,0,0);
+//   ctx.drawImage(pipeNorth,50,0);
+//   ctx.drawImage(pipeSouth,50,0+contant);
+//   ctx.drawImage(fg,0,canvas.height-fg.height);
+//   ctx.drawImage(bird,bX,bY);
+//   bY+= gravity;
+// }
