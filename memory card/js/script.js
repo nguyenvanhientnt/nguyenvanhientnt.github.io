1631175fb7e8c04abb30e0e9788b88222df5e68b
nguyenvanhientnt <hanoimatbao@gmail.com>
2018-09-04 15:11:07 +0700

    creat memory card

--- memory card/js/script.js
+++ memory card/js/script.js
@@ -1,6 +1,9 @@
 let img = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16'];
 let current = null;
-
+let count = 0;
+let remainingTime = 95;
+let run;
+ // document.getElementById('bg-sound').play();
 function flip(card){
     $(card).toggleClass('flipped');
     document.getElementById('flip-sound').play();
@@ -13,20 +16,14 @@ function flip(card){
         if(current.attr('data-name') != $(card).attr('data-name')){
             //khác nhau
             $('.card').css('pointer-events', 'none');
-
             setTimeout(function(){
                 $(card).toggleClass('flipped');
                 current.toggleClass('flipped');
-                document.getElementById('incorrect-sound').play();
+                // document.getElementById('incorrect-sound').play();
                 current = null;
             }, 500);
-            
-            console.log('khac nhau');
             setTimeout(function(){
                 $('.card').css('pointer-events', 'auto');
-                
-            console.log('khac nhau2');
-
             }, 800);
         }else{
             // giống nhau
@@ -37,10 +34,51 @@ function flip(card){
                 current.css('opacity', '0');
                 document.getElementById('correct-sound').play();
                 current = null;
+                count++;
+                if(count == 16){
+                    clearInterval(run);
+                    $('#winner').css('display', 'block');
+                    $('#time').css('display', 'none');
+                    document.getElementById('win-sound').play();
+                    document.getElementById('bg-sound').pause();
+                }
             }, 200);
-            // document.getElementById('').play();
         }
     }
+}
+function normal(){
+    $('.can').css('display', 'block');
+    $('.begins').css('display', 'none');
+    run = setInterval(function(){
+        remainingTime--;
+        document.getElementById('time').value = remainingTime;
+        document.getElementById('bg-sound').play();
+        if(remainingTime == 0){
+            clearInterval(run);
+            $('.can').css('display', 'none');
+            $('#lose').css('display', 'block');
+            $('#time').css('display', 'none');
+            document.getElementById('lose-sound').play();
+            document.getElementById('bg-sound').pause();  
+        }
+    }, 1000);
+}
+function hard(){
+    $('.can').css('display', 'block');
+    $('.begins').css('display', 'none');
+    run = setInterval(function(){
+        remainingTime--;
+        document.getElementById('time').value = remainingTime;
+        document.getElementById('bg-sound').play();
+        if(remainingTime == 0){
+            clearInterval(run);
+            $('.can').css('display', 'none');
+            $('#lose').css('display', 'block');
+            $('#time').css('display', 'none');
+            document.getElementById('lose-sound').play();
+            document.getElementById('bg-sound').pause();
+        }
+    }, 700);
 }
 
 $(function(){
@@ -53,20 +91,22 @@ $(function(){
     $('.can').html(html);
 });
 
+
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-
         // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-
         // And swap it with the current element.
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
   }
   return array;
+}
+
+function comeback(){
+    location.reload();
 }
