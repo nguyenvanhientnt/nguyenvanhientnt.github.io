eee942fa4f456c1064e6ad4e747c13b7809cc474
nguyenvanhientnt <hanoimatbao@gmail.com>
2018-09-21 23:36:08 +0700

    creat table with sort table

--- table/js/script.js
+++ table/js/script.js
@@ -17,7 +17,7 @@ function sortTable(n) {
       
       x = rows[i].getElementsByTagName("TD")[n];
       y = rows[i + 1].getElementsByTagName("TD")[n];
-     
+      console.log(rows);
       if (dir == "asc") {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
          
@@ -26,6 +26,93 @@ function sortTable(n) {
         }
       } else if (dir == "desc") {
         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+     
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      
+      switchcount ++;      
+    } else {
+      
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+// function sort(n) {
+//   var table, rows, switching, i, x, y, shouldSwitch, switchcount = 0;
+//   table = document.getElementById("myTable");
+//   switching = true;
+//   while (switching) {
+    
+//     switching = false;
+//     rows = table.rows;
+    
+//     for (i = 1; i < (rows.length - 1); i++) {
+      
+//       shouldSwitch = false;
+      
+//       x = rows[i].getElementsByTagName("TD")[n];
+//       y = rows[i + 1].getElementsByTagName("TD")[n];
+//       console.log(rows);
+//       if (Number(x.innerHTML) > Number(y.innerHTML)) {
+        
+//         shouldSwitch = true;
+//         break;
+//       }else{
+//       		if (Number(x.innerHTML) < Number(x.innerHTML)) {
+// 		    	shouldSwitch = true;
+// 		        break;
+// 		    }
+//       }
+//     }
+//     if (shouldSwitch) {
+//       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+//       switching = true;
+//       switchcount++;
+//     }else{
+//     	if (switchcount == 0 && Number(x.innerHTML) < Number(x.innerHTML)) {
+//         switching = true;
+//       }
+//     }
+//   }
+// }
+
+function sort(n) {
+  let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable");
+  switching = true;
+  
+  dir = "asc"; 
+
+  while (switching) {
+    
+    switching = false;
+    rows = table.rows;
+    
+    for (i = 1; i < (rows.length - 1); i++) {
+      
+      shouldSwitch = false;
+      
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      console.log(rows);
+      if (dir == "asc") {
+        if (Number(x.innerHTML) > Number(y.innerHTML)) {
+         
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (Number(x.innerHTML) < Number(y.innerHTML)) {
           
           shouldSwitch = true;
           break;
